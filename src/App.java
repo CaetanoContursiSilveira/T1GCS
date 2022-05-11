c915ece5128aa8caa983c4bf66774a10a478246b
Rodrigo Rech <rodrigohrech@gmail.com>
2022-05-11 20:08:12 -0300

    adiciona: funcionalidade de criar comentario e mostrar comentarios quando lista as postagens

--- src/App.java
+++ src/App.java
@@ -5,11 +5,22 @@ import java.util.ArrayList;
 
 public class App {
 
+    private static ArrayList<Post> postagensInicial() {
+        User user = new User("John Doe", false);
+        return new ArrayList<Post>() {
+            {
+                add(new Post(1, user, "Texto da Postagem 1"));
+                add(new Post(2, user, "Texto da Postagem 2"));
+                add(new Post(3, user, "Texto da Postagem 3"));
+            }
+        };
+    }
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in));
-        Menu menu = new Menu(reader);
+        Menu menu = new Menu(reader, postagensInicial());
 
 
         System.out.println("Bem Vindo ao Blog Interno de T1GCS, digite o número da opção desejada:");
@@ -17,17 +28,21 @@ public class App {
         System.out.println("2 - Criar uma nova postagem");
         System.out.println("3 - Listar postagens");
         System.out.println("4 - Excluir postagem");
-        System.out.println("5 - Sair");
+        System.out.println("5 - Criar comentário");
+        System.out.println("6 - Sair");
 
         // Reading data using readLine
         Integer opcao = Integer.parseInt(reader.readLine());
-        while (opcao != 5) {
+        while (opcao != 6) {
             switch (opcao) {
                 case 2:
                     menu.criarNovaPostagem();
                     break;
                 case 3:
                     menu.listarPostagens();
+                    break;
+                case 5:
+                    menu.criarComentario();
                     break;
             }
             System.out.println("Digite nova opção:");
